d4876375fb5fda2a97fd71f2e472e5107ed6f439
Elton Bautista <eltonbestaha@gmail.com>
2022-02-22 10:16:01 -0700

    wrote up the logic for generating divs, need to figure out how to select player

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const gameBoard = (() => {
     let gameboard = [];
+    const gridBoard = document.getElementById('grid-board');
     
+
+    const createGridBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            const gridCell = document.createElement('div');
+            gridCell.dataset.cellPosition = i;
+            gameboard.push(gridCell);
+            gridBoard.appendChild(gridCell);
+        }
+    }
+
+    return {
+        createGridBoard,
+        gameboard
+    };
+
 })();
 
 
